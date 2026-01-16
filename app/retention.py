ae2fc5936721a2b0f637513981ad0880aeccf928
Apollomakesit <138776863+Apollomakesit@users.noreply.github.com>
2026-01-17 01:52:17 +0200

    Delete app directory

--- app/retention.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-from sqlalchemy import delete
-from models import History
-from config import DATA_RETENTION_DAYS
-
-async def cleanup(db):
-    cutoff = datetime.utcnow() - timedelta(days=DATA_RETENTION_DAYS)
-    await db.execute(delete(History).where(History.recorded_at < cutoff))
-    await db.commit()
